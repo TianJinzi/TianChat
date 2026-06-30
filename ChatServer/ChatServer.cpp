a3e7e282d91fcf501bdcd0a80e280e426678a117
tian <2716514148@qq.com>
2026-06-30 15:31:20 +0800

    修复了踢人的时候崩溃的bug

--- ChatServer/ChatServer.cpp
+++ ChatServer/ChatServer.cpp
@@ -26,7 +26,17 @@ int main()
 	try {
 		auto pool = AsioIOServicePool::GetInstance();
 		//将登录数设置为0
-		RedisMgr::GetInstance()->HSet(LOGIN_COUNT, server_name,"0");
+		RedisMgr::GetInstance()->InitCount(server_name);
+		Defer derfer([server_name]() {
+			RedisMgr::GetInstance()->HDel(LOGIN_COUNT, server_name);
+			RedisMgr::GetInstance()->Close();
+			});
+
+		boost::asio::io_context  io_context;
+		auto port_str = cfg["SelfServer"]["Port"];
+		//创建Cserver智能指针
+		auto pointer_server = std::make_shared<CServer>(io_context, atoi(port_str.c_str()));
+
 
 		//定义一个GrpcServer
 
@@ -36,56 +46,33 @@ int main()
 		// 监听端口和添加服务
 		builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
 		builder.RegisterService(&service);
+		service.RegisterServer(pointer_server);
 		// 构建并启动gRPC服务器
 		std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-
-		if (server.get() == nullptr) {
-			std::cout << "grpc server is null!\n";
-			return -1;
-		}
-
 		std::cout << "RPC Server listening on " << server_address << std::endl;
 
 		//单独启动一个线程处理grpc服务
 		std::thread  grpc_server_thread([&server]() {
-				server->Wait();
+			server->Wait();
 			});
 
-		boost::asio::io_context  io_context;
+
 		boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
 		signals.async_wait([&io_context, pool, &server](auto, auto) {
 			io_context.stop();
 			pool->Stop();
 			server->Shutdown();
 			});
-		auto port_str = cfg["SelfServer"]["Port"];
-		CServer s(io_context, atoi(port_str.c_str()));
-		////中间是新增的定时器逻辑
-		//boost::asio::deadline_timer timer(io_context);
-		//std::function<void(const boost::system::error_code&)> timer_cb;
-		//timer_cb = [&timer,&s,&timer_cb](const boost::system::error_code& ec) {
-		//	//此时是主动cancel取消定时器，直接返回
-		//	if (ec) {
-		//		return;
-		//	}
-		//	//这里检查所有的连接，进行处理
 
-		//	timer.expires_from_now(boost::posix_time::seconds(60));
-		//	timer.async_wait(timer_cb);
-		//};
-		//// 第一次启动定时，注册第一轮等待
-		//timer.expires_from_now(boost::posix_time::seconds(60));
-		//timer.async_wait(timer_cb);
-		///////
+
+		//将Cserver注册给逻辑类方便以后清除连接
+		LogicSystem::GetInstance()->SetServer(pointer_server);
 		io_context.run();
-		RedisMgr::GetInstance()->HDel(LOGIN_COUNT, server_name);
-		RedisMgr::GetInstance()->Close();
+
 		grpc_server_thread.join();
 	}
 	catch (std::exception& e) {
 		std::cerr << "Exception: " << e.what() << endl;
-		RedisMgr::GetInstance()->HDel(LOGIN_COUNT, server_name);
-		RedisMgr::GetInstance()->Close();
 	}
 	
 }
